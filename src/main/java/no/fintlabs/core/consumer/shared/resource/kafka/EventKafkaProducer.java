7af1b105b1bcaf01a00b59246c21f8dfc5afcde4
Trond Magnus Sevre <trond@fintlabs.no>
2022-11-02 10:51:27 +0100

    Optional headers + ensure topic

--- src/main/java/no/fintlabs/core/consumer/shared/resource/kafka/EventKafkaProducer.java
+++ src/main/java/no/fintlabs/core/consumer/shared/resource/kafka/EventKafkaProducer.java
@@ -6,16 +6,22 @@ import no.fintlabs.kafka.event.EventProducer;
 import no.fintlabs.kafka.event.EventProducerFactory;
 import no.fintlabs.kafka.event.EventProducerRecord;
 import no.fintlabs.kafka.event.topic.EventTopicNameParameters;
+import no.fintlabs.kafka.event.topic.EventTopicService;
 
 public class EventKafkaProducer {
+
+    private static final int RETENTION_TIME_MS = 172800000;
 
     private final EventProducer<Object> eventProducer;
 
     private final ConsumerConfig consumerConfig;
 
-    public EventKafkaProducer(EventProducerFactory eventProducerFactory, ConsumerConfig consumerConfig) {
+    private final EventTopicService eventTopicService;
+
+    public EventKafkaProducer(EventProducerFactory eventProducerFactory, ConsumerConfig consumerConfig, EventTopicService eventTopicService) {
         this.consumerConfig = consumerConfig;
         this.eventProducer = eventProducerFactory.createProducer(Object.class);
+        this.eventTopicService = eventTopicService;
     }
 
     public void sendEvent(RequestFintEvent<?> event, OperationType operationType) {
@@ -27,14 +33,18 @@ public class EventKafkaProducer {
                 operationType.equals(OperationType.CREATE) ? "create" : "update",
                 "request");
 
+        EventTopicNameParameters topicNameParameters = EventTopicNameParameters
+                .builder()
+                .orgId(consumerConfig.getOrgId())
+                .domainContext("fint-core")
+                .eventName(eventName)
+                .build();
+
+        eventTopicService.ensureTopic(topicNameParameters, RETENTION_TIME_MS);
+
         eventProducer.send(
-                EventProducerRecord.<Object>builder()
-                        .topicNameParameters(EventTopicNameParameters
-                                .builder()
-                                .orgId(consumerConfig.getOrgId())
-                                .domainContext("fint-core")
-                                .eventName(eventName)
-                                .build())
+                EventProducerRecord.builder()
+                        .topicNameParameters(topicNameParameters)
                         .value(event)
                         .build()
         );
