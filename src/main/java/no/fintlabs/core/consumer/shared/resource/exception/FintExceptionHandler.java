b30e8b4cf6e48975dae0c8ba9eebba6113d4cdd5
Hknots <Hknots99@gmail.com>
2023-03-20 13:14:01 +0100

    removed import

--- src/main/java/no/fintlabs/core/consumer/shared/resource/exception/FintExceptionHandler.java
+++ src/main/java/no/fintlabs/core/consumer/shared/resource/exception/FintExceptionHandler.java
@@ -1,7 +1,5 @@
 package no.fintlabs.core.consumer.shared.resource.exception;
-
 import no.fint.antlr.exception.FilterException;
-import no.fint.cache.exceptions.CacheNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
